4bdbc584a5d8db869ba63c5f96b808a4c13bbbfb
Radoslav Klein <klein.radoslav.1992@gmail.com>
2026-08-24 10:11:46 +0200

    dede

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import AppTest from './AppTest.jsx'
-import DutchGame from './DutchGame.jsx'
 
-const path = window.location.pathname;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {path === '/test' ? <AppTest /> : path === '/DutchGame' ? <DutchGame /> : <App />}
+    <App />
   </StrictMode>,
 )
